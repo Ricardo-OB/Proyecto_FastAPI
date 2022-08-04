3982aaf34e0215949894db70f1e3c74b5719069d
Ricardo Ortega Bolaños <ricardorob587@gmail.com>
2022-08-03 22:01:00 -0500

    Creando archivo Procfile

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ app = FastAPI()
 app.include_router(user.router_1)
 app.include_router(auth.router_2)
 
-# Main
-if __name__=='__main__':
-    uvicorn.run('main:app', port=8000, reload=True)
+# if __name__=='__main__':
+#     uvicorn.run('main:app', port=8000, reload=True)
+
